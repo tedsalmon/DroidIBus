426ba4f654b407a731e8ce6ad9802573da9a1b6c
Ted <tass2001@hotmail.com>
2014-05-20 23:14:43 -0700

    Revert to regular ENums

--- src/net/littlebigisland/droidibus/ibus/IBusCommands.java
+++ src/net/littlebigisland/droidibus/ibus/IBusCommands.java
@@ -1,22 +1,8 @@
 package net.littlebigisland.droidibus.ibus;
 
 public enum IBusCommands {
-	
-	IKEGetFuel(DeviceAddress.InstrumentClusterElectronics.toByte(), 0);
-	
-	private final byte targetSystem;
-	private final int functionID;
-	
-	IBusCommands(byte ts, int fid){
-		targetSystem = ts;
-		functionID = fid;
-	}
-	
-	public byte getTargetSystem(){
-		return targetSystem;
-	}
-	
-	public int getFunctionID(){
-		return functionID;
-	}
+	IKEGetFuel1,
+	IKEResetFuel1,
+	IKEGetFuel2,
+	IKEResetFuel2;
 }
