02bf3d914675a9915c2f50050c56234186fde849
Ted <tass2001@hotmail.com>
2014-08-06 00:52:44 -0700

    Added support for action bar

--- src/net/littlebigisland/droidibus/activity/MainActivity.java
+++ src/net/littlebigisland/droidibus/activity/MainActivity.java
@@ -9,7 +9,9 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,6 +20,7 @@ import android.widget.ListView;
 public class MainActivity extends Activity {
 	private DrawerLayout mDrawerLayout = null;
 	private ListView mDrawerList = null;
+	private ActionBarDrawerToggle mDrawerToggle;
 	
 	private Fragment[] mFragments = {
 		new DashboardFragment(),
@@ -25,11 +28,17 @@ public class MainActivity extends Activity {
 		new SettingsFragment(),
 	};
 	
+	private CharSequence[] mFragmentTitles = {
+		"Dashboard",
+		"Navigation",
+		"Settings"
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		 super.onCreate(savedInstanceState);
-		 setContentView(R.layout.main_activity);
-		 
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+		
 		// Take care of the drawer
 		mDrawerList = (ListView) findViewById(R.id.left_drawer);
 		
@@ -37,15 +46,61 @@ public class MainActivity extends Activity {
 		
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+            // Called when a drawer has settled in a completely closed state.
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            // Called when a drawer has settled in a completely open state.
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+            
+        };
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+
+        //Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+		getActionBar().setIcon(R.drawable.ic_drawer);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
 		// set up the drawer's list view with items and click listener
 		mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_item, mSettingsItems));
 		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 		if(savedInstanceState == null){
 			FragmentTransaction fragmentTx = getFragmentManager().beginTransaction();
 			fragmentTx.replace(R.id.main, mFragments[0]);
+			getActionBar().setDisplayShowTitleEnabled(true);
+			getActionBar().setTitle(mFragmentTitles[0]);
 			fragmentTx.addToBackStack(null);
 			fragmentTx.commit();
 		}
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    	case android.R.id.home:
+		        if(mDrawerLayout.isDrawerOpen(mDrawerList)) {
+		        	mDrawerLayout.closeDrawer(mDrawerList);
+		        }
+		        else {
+		        	mDrawerLayout.openDrawer(mDrawerList);
+		        }
+		        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
 	}
 	
     /* The click listener for ListView in the navigation drawer */
@@ -56,6 +111,8 @@ public class MainActivity extends Activity {
         		@Override
         		public void onDrawerClosed(View drawerView){
         			super.onDrawerClosed(drawerView);
+        			getActionBar().setDisplayShowTitleEnabled(true);
+        			getActionBar().setTitle(mFragmentTitles[position]);
         			FragmentTransaction fragmentTx = getFragmentManager().beginTransaction();
         			fragmentTx.replace(R.id.main, mFragments[position]);
         			fragmentTx.addToBackStack(null);
