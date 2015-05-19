fa567677315ff220e30c2f5e5cdf24f6e75fb451
t3ddftw <tass2001@gmail.com>
2015-05-19 02:12:44 -0700

    #20 #time 10m #comment Linked Navigation option to GMaps;

--- src/net/littlebigisland/droidibus/activity/MainActivity.java
+++ src/net/littlebigisland/droidibus/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package net.littlebigisland.droidibus.activity;
 /**
- * @author Ted S <tass2001@gmail.com>
+ * 
+ * @author Ted <tass2001@gmail.com>
  * @package net.littlebigisland.droidibus
  *
  */
@@ -10,6 +11,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentManager.OnBackStackChangedListener;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
@@ -20,49 +22,49 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
-	private DrawerLayout mDrawerLayout = null;
-	private ListView mDrawerList = null;
-	private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout = null;
+    private ListView mDrawerList = null;
+    private ActionBarDrawerToggle mDrawerToggle;
+    
+    private Fragment[] mFragments = {
+        new DashboardFragment(),
+        new SettingsFragment(),
+        new SettingsFragment(),
+    };
+    
+    private String[] mFragmentTitles = {
+        "Dashboard",
+        "Navigation",
+        "Settings"
+    };
 	
-	private Fragment[] mFragments = {
-		new DashboardFragment(),
-		new SettingsFragment(),
-		new SettingsFragment(),
-	};
-	
-	private String[] mFragmentTitles = {
-		"Dashboard",
-		"Navigation",
-		"Settings"
-	};
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_activity);
-		
-		// Take care of the drawer
-		mDrawerList = (ListView) findViewById(R.id.left_drawer);
-		
-		String[] mSettingsItems = getResources().getStringArray(R.array.options_array);
-		
-		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        
+        // Take care of the drawer
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        
+        String[] mSettingsItems = getResources().getStringArray(R.array.options_array);
+        
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-		mDrawerToggle = new ActionBarDrawerToggle(
-			this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close
-		){
-			// Called when a drawer has settled in a completely closed state.
-			public void onDrawerClosed(View view) {
-			    super.onDrawerClosed(view);
-			    invalidateOptionsMenu();
-			}
-			// Called when a drawer has settled in a completely open state.
-			public void onDrawerOpened(View drawerView) {
-			    super.onDrawerOpened(drawerView);
-			    invalidateOptionsMenu();
-			}
-		    
-		};
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close
+        ){
+                // Called when a drawer has settled in a completely closed state.
+                public void onDrawerClosed(View view) {
+                    super.onDrawerClosed(view);
+                    invalidateOptionsMenu();
+                }
+                // Called when a drawer has settled in a completely open state.
+                public void onDrawerOpened(View drawerView) {
+                    super.onDrawerOpened(drawerView);
+                    invalidateOptionsMenu();
+                }
+                
+        };
         mDrawerLayout.post(new Runnable() {
             @Override
             public void run() {
@@ -72,54 +74,54 @@ public class MainActivity extends Activity {
 
         //Set the drawer toggle as the DrawerListener
         mDrawerLayout.setDrawerListener(mDrawerToggle);
-		getActionBar().setIcon(R.drawable.ic_drawer);
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-		
-		// set up the drawer's list view with items and click listener
-		mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_item, mSettingsItems));
-		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-		if(savedInstanceState == null){
-			// This callback sets the fragment title every time the fragment changes
-			getFragmentManager().addOnBackStackChangedListener(new OnBackStackChangedListener() {
-				public void onBackStackChanged() {
-					FragmentManager manager = getFragmentManager();
-					if (manager != null) {
-						int backStackSize = manager.getBackStackEntryCount();
-						if(backStackSize != 0){
-							FragmentManager.BackStackEntry backEntry = getFragmentManager().getBackStackEntryAt(
-								backStackSize - 1
-							);
-							getActionBar().setTitle(backEntry.getName());
-						}else{
-							finish(); // Go back to the launcher since this is the first fragment
-						}
-					}
-				}
-			});
-			// Add the main fragment.
-			FragmentTransaction fragmentTx = getFragmentManager().beginTransaction();
-			fragmentTx.replace(R.id.main, mFragments[0]);
-			getActionBar().setDisplayShowTitleEnabled(true);
-			fragmentTx.addToBackStack(mFragmentTitles[0]);
-			fragmentTx.commit();
-		}
-	}
+        getActionBar().setIcon(R.drawable.ic_drawer);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_item, mSettingsItems));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        if(savedInstanceState == null){
+            // This callback sets the fragment title every time the fragment changes
+            getFragmentManager().addOnBackStackChangedListener(new OnBackStackChangedListener() {
+                public void onBackStackChanged() {
+                    FragmentManager manager = getFragmentManager();
+                    if (manager != null) {
+                        int backStackSize = manager.getBackStackEntryCount();
+                        if(backStackSize != 0){
+                            FragmentManager.BackStackEntry backEntry = getFragmentManager().getBackStackEntryAt(
+                                    backStackSize - 1
+                            );
+                            getActionBar().setTitle(backEntry.getName());
+                        }else{
+                            finish(); // Go back to the launcher since this is the first fragment
+                        }
+                    }
+                }
+            });
+            // Add the main fragment.
+            FragmentTransaction fragmentTx = getFragmentManager().beginTransaction();
+            fragmentTx.replace(R.id.main, mFragments[0]);
+            getActionBar().setDisplayShowTitleEnabled(true);
+            fragmentTx.addToBackStack(mFragmentTitles[0]);
+            fragmentTx.commit();
+        }
+    }
 	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    switch (item.getItemId()) {
-	    	case android.R.id.home:
-		        if(mDrawerLayout.isDrawerOpen(mDrawerList)) {
-		        	mDrawerLayout.closeDrawer(mDrawerList);
-		        }
-		        else {
-		        	mDrawerLayout.openDrawer(mDrawerList);
-		        }
-		        return true;
-	    default:
-	        return super.onOptionsItemSelected(item);
-	    }
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if(mDrawerLayout.isDrawerOpen(mDrawerList)) {
+                    mDrawerLayout.closeDrawer(mDrawerList);
+                }
+                else {
+                    mDrawerLayout.openDrawer(mDrawerList);
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 	
     /**
      *  The click listener for ListView in the navigation drawer
@@ -127,18 +129,29 @@ public class MainActivity extends Activity {
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-        	mDrawerLayout.setDrawerListener( new DrawerLayout.SimpleDrawerListener(){
-        		@Override
-        		public void onDrawerClosed(View drawerView){
-        			super.onDrawerClosed(drawerView);
-        			getActionBar().setDisplayShowTitleEnabled(true);
-        			FragmentTransaction fragmentTx = getFragmentManager().beginTransaction();
-        			fragmentTx.replace(R.id.main, mFragments[position]);
-        			fragmentTx.addToBackStack(mFragmentTitles[position]);
-        			fragmentTx.commit();
-        		}
-        	});
-        	mDrawerLayout.closeDrawer(mDrawerList);
+            mDrawerLayout.setDrawerListener( new DrawerLayout.SimpleDrawerListener(){
+                @Override
+                public void onDrawerClosed(View drawerView){
+                    super.onDrawerClosed(drawerView);
+                    getActionBar().setDisplayShowTitleEnabled(true);
+                    FragmentTransaction fragmentTx = getFragmentManager().beginTransaction();
+                    String fragmentTitle = mFragmentTitles[position];
+                    if(fragmentTitle.equals("Navigation")){
+                    	Intent intent = new Intent();
+                    	intent.setClassName(
+                    		"com.google.android.apps.maps",
+                    		"com.google.android.maps.MapsActivity"
+                    	); 
+                    	intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    	startActivity(intent);
+                    }else{
+	                    fragmentTx.replace(R.id.main, mFragments[position]);
+	                    fragmentTx.addToBackStack(fragmentTitle);
+	                    fragmentTx.commit();
+                    }
+                }
+            });
+            mDrawerLayout.closeDrawer(mDrawerList);
         }
     }
 }
