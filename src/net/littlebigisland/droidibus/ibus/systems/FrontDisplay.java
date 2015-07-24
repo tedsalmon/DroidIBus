4da688c5228819a6dcf4af250ead67d7d48b6cf5
t3ddftw <tass2001@gmail.com>
2015-07-24 13:35:39 -0700

    #52 #comment Extended the wrong IKE Subsystem. Whoops.

--- src/net/littlebigisland/droidibus/ibus/systems/FrontDisplay.java
+++ src/net/littlebigisland/droidibus/ibus/systems/FrontDisplay.java
@@ -1,11 +1,11 @@
 package net.littlebigisland.droidibus.ibus.systems;
 
-public class FrontDisplay extends GlobalBroadcastSystem{
+public class FrontDisplay extends BroadcastSystem{
     
     /**
      * FrontDisplay is a system used on older E3x models
      * 
-     * FrontDisplay extends GlobalBroadcast because the IKE functionality
+     * FrontDisplay extends Broadcast System because the IKE functionality
      * is exactly the same. We must however clear the Destination systems
      * and add the ones we need as Java parent construct calls are implicit.
      */
